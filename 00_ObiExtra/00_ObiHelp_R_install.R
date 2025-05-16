1aef5880f14c8daecc0687e74d29c0bc86a04b07
Tilman Schell <schell.tilman@gmail.com>
2025-05-16 10:11:54 +0200

    Update 00_ObiHelp_R_install.R
    ROBIFastread link from github

--- 00_ObiExtra/00_ObiHelp_R_install.R
+++ 00_ObiExtra/00_ObiHelp_R_install.R
@@ -2,7 +2,7 @@
 
 install.packages("jsonify", repos='http://cran.us.r-project.org')
 install.packages("devtools", repos='http://cran.us.r-project.org')
-devtools::install_git("https://git.metabarcoding.org/obitools/obitools4/robireadfasta.git")
+devtools::install_git("https://github.com/metabarcoding/ROBIFastread")
 
 install.packages(c("cowplot","dplyr","gapminder","ggplot2","ggpmisc","ggpubr","ghibli","magrittr","plotly","seqinr","taxize","taxonomizr","tidyverse","treemapify","vegan"),repos='http://cran.us.r-project.org')
 
